1741d0545bbac6f6241149aa38fc92d681ae0370
mohamedosama2074 <iosengks74@gmail.com>
2019-09-21 14:27:53 +0200

    Initial Commit

---
+++ AlamofireWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AlamofireWeather
+//
+//  Created by Mohamed on 9/21/19.
+//  Copyright © 2019 Mohamed74. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
